83ecff9421cd8b352ec1313bad2bf1c6cb55db82
vandelshtam <mvlju977@gmail.com>
2022-04-13 08:54:44 +0300

    37 commit

--- comet-affiliate_program.sql
+++ comet-affiliate_program.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:8889
--- Время создания: Апр 02 2022 г., 08:38
+-- Время создания: Апр 13 2022 г., 05:53
 -- Версия сервера: 5.7.34
 -- Версия PHP: 8.0.8
 
@@ -136,7 +136,38 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20220401130857', '2022-04-01 13:09:01', 22),
 ('DoctrineMigrations\\Version20220401131505', '2022-04-01 13:15:08', 16),
 ('DoctrineMigrations\\Version20220401132019', '2022-04-01 13:20:23', 56),
-('DoctrineMigrations\\Version20220402070255', '2022-04-02 07:03:01', 22);
+('DoctrineMigrations\\Version20220402070255', '2022-04-02 07:03:01', 22),
+('DoctrineMigrations\\Version20220404073435', '2022-04-04 07:34:52', 91),
+('DoctrineMigrations\\Version20220404130428', '2022-04-04 13:04:34', 19),
+('DoctrineMigrations\\Version20220404131809', '2022-04-04 13:18:13', 18),
+('DoctrineMigrations\\Version20220405060611', '2022-04-05 06:06:15', 80),
+('DoctrineMigrations\\Version20220405065525', '2022-04-05 06:55:29', 19),
+('DoctrineMigrations\\Version20220405130719', '2022-04-05 13:07:26', 85),
+('DoctrineMigrations\\Version20220405132312', '2022-04-05 13:23:17', 18),
+('DoctrineMigrations\\Version20220406122550', '2022-04-06 12:25:58', 50),
+('DoctrineMigrations\\Version20220406133320', '2022-04-06 13:33:24', 45),
+('DoctrineMigrations\\Version20220406133529', '2022-04-06 13:35:33', 18),
+('DoctrineMigrations\\Version20220407125044', '2022-04-07 12:50:48', 95),
+('DoctrineMigrations\\Version20220407130021', '2022-04-07 13:00:26', 47),
+('DoctrineMigrations\\Version20220408072638', '2022-04-08 07:26:43', 101),
+('DoctrineMigrations\\Version20220411114657', '2022-04-11 11:47:13', 69),
+('DoctrineMigrations\\Version20220411121212', '2022-04-11 12:12:17', 115),
+('DoctrineMigrations\\Version20220411121552', '2022-04-11 12:15:58', 20),
+('DoctrineMigrations\\Version20220412095325', '2022-04-12 09:53:30', 87);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `fast_consultation`
+--
+
+CREATE TABLE `fast_consultation` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `question` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -159,6 +190,13 @@ CREATE TABLE `list_referral_networks` (
   `payments_cash` int(11) DEFAULT NULL,
   `current_balance` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `list_referral_networks`
+--
+
+INSERT INTO `list_referral_networks` (`id`, `name`, `referral_networks_id`, `owner_id`, `owner_name`, `pakege_id`, `unique_code`, `network_code`, `client_code`, `profit_network`, `payments_direct`, `payments_cash`, `current_balance`) VALUES
+(1, 'My Network 1', NULL, 9, NULL, 313, 'JAjHikwlH7K0Nl2I19QN', '313-JAjHikwlH7K0Nl2I19QN', '9CP907927910', 12750, 1250, 1000, 9998);
 
 -- --------------------------------------------------------
 
@@ -189,6 +227,16 @@ CREATE TABLE `pakege` (
   `unique_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `referral_link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `pakege`
+--
+
+INSERT INTO `pakege` (`id`, `name`, `user_id`, `price`, `referral_networks_id`, `client_code`, `token`, `activation`, `unique_code`, `referral_link`) VALUES
+(313, 'VIP', 9, 10000, '1-313-313-JAjHikwlH7K0Nl2I19QN', '9CP907927910', 200000, 'активирован', 'JAjHikwlH7K0Nl2I19QN', NULL),
+(314, 'Trader', 10, 5000, '313-JAjHikwlH7K0Nl2I19QN', '10CP274970529', 100000, 'активирован', 'buXQ1A9hJ86bgXCbHYnA', '1-313-313-JAjHikwlH7K0Nl2I19QN'),
+(315, 'VIP', 11, 10000, '313-JAjHikwlH7K0Nl2I19QN', '11CP1988326848', 200000, 'активирован', 'KXbWeTRfNv3tyU1VAY9B', '1-313-313-JAjHikwlH7K0Nl2I19QN'),
+(316, 'Business', 12, 2500, '313-JAjHikwlH7K0Nl2I19QN', '12CP1927876844', 50000, 'активирован', 'pYyqFPhW3skwndqZBUMb', '1-314-313-JAjHikwlH7K0Nl2I19QN');
 
 -- --------------------------------------------------------
 
@@ -221,9 +269,10 @@ CREATE TABLE `personal_data` (
 --
 
 INSERT INTO `personal_data` (`id`, `name`, `surname`, `phone`, `state`, `region`, `city`, `street`, `house`, `frame`, `apartment`, `indexcity`, `user_id`, `users_id`, `tel`, `block`, `client_code`) VALUES
-(35, 'Vladimir', 'Morozov', '89957771877', 'Armenia', 'Dfg', 'Москва', 'Первомайское, Горчаково, Школьная Улица, Д.11, К.2', 3, '6', 3, 143396, 3, NULL, NULL, '6', '32010652754'),
-(40, 'Nnnn', 'Nnnnn', '8985756748', 'Armenia', 'vvv', 'bbb', 'gfddd', 1, NULL, 3, 789654, 2, NULL, NULL, 'c', '21497043565'),
-(41, 'vvvvv', 'bbbbbb', '666666666', 'Georgia', 'hhhh', 'kkkkkk', 'wwwwww', 2, NULL, 4, 876543, 11, NULL, NULL, '3', '11155039691');
+(52, 'xxx', 'xxx', '3333333', 'Armenia', 'xxx', 'xxx', 'xxx', 33, NULL, 3, 3333, 9, NULL, NULL, '3', '9CP907927910'),
+(53, 'aaaaa', 'aaaaaa', '3333333', 'Armenia', 'aaaaa', 'aaaaaa', 'aaaaa', 33, NULL, 3, 33333, 10, NULL, NULL, '3', '10CP274970529'),
+(54, 'hhh', 'hhhh', '44444', 'Armenia', 'hhhhh', 'hhhhh', 'hhhh', 44, NULL, 44, 4444, 11, NULL, NULL, '4', '11CP1988326848'),
+(55, '555', '5555', '5555', 'Armenia', '5555', '5555', '5555', 5555, NULL, 55, 5555, 12, NULL, NULL, '5555', '12CP1927876844');
 
 -- --------------------------------------------------------
 
@@ -234,7 +283,8 @@ INSERT INTO `personal_data` (`id`, `name`, `surname`, `phone`, `state`, `region`
 CREATE TABLE `pkege` (
   `id` int(11) NOT NULL,
   `activation` int(11) DEFAULT NULL,
-  `token` int(11) DEFAULT NULL
+  `token` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -264,7 +314,32 @@ CREATE TABLE `referral_network` (
   `current_network_profit` int(11) DEFAULT NULL,
   `payments_network` int(11) DEFAULT NULL,
   `payments_cash` int(11) DEFAULT NULL,
-  `pakage` int(11) DEFAULT NULL
+  `pakage` int(11) DEFAULT NULL,
+  `reward_wallet` int(11) DEFAULT NULL,
+  `withdrawal_to_wallet` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `referral_network`
+--
+
+INSERT INTO `referral_network` (`id`, `name`, `user_id`, `user_status`, `personal_data_id`, `balance`, `network_code`, `member_code`, `pakege_id`, `network_id`, `user_referral_id`, `network_referral_id`, `reward`, `koef`, `cash`, `direct`, `my_team`, `current_network_profit`, `payments_network`, `payments_cash`, `pakage`, `reward_wallet`, `withdrawal_to_wallet`) VALUES
+(1, NULL, 9, 'owner', NULL, 6666, '313-JAjHikwlH7K0Nl2I19QN', '1-313-313-JAjHikwlH7K0Nl2I19QN', 313, 1, NULL, NULL, 2500, 0.66669333546684, 1000, 1500, '1-313-313-JAjHikwlH7K0Nl2I19QN', 0, 0, 0, 10000, 2500, 0),
+(3, 'My name 1', 10, 'left', NULL, 0, '313-JAjHikwlH7K0Nl2I19QN', '1-314-313-JAjHikwlH7K0Nl2I19QN', 314, 1, 9, 1, 1000, NULL, 500, 500, '1-313-313-JAjHikwlH7K0Nl2I19QN', 0, 0, 0, 5000, 1000, 0),
+(4, 'My name 7', 11, 'right', NULL, 3332, '313-JAjHikwlH7K0Nl2I19QN', '1-315-313-JAjHikwlH7K0Nl2I19QN', 315, 1, 9, 1, 0, 0.33330666453316, 0, 0, '1-313-313-JAjHikwlH7K0Nl2I19QN', 8500, 1000, 500, 10000, 0, 0),
+(10, 'Uhfgft', 12, 'left', NULL, 0, '313-JAjHikwlH7K0Nl2I19QN', '1-316-313-JAjHikwlH7K0Nl2I19QN', 316, 1, 10, 3, 0, NULL, 0, 0, '1-314-313-JAjHikwlH7K0Nl2I19QN', 4250, 250, 500, 2500, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `referral_to_email`
+--
+
+CREATE TABLE `referral_to_email` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `referral_link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_to_client` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -383,16 +458,22 @@ CREATE TABLE `user` (
   `personal_data_id` int(11) DEFAULT NULL,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `referral_link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `referral_link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pesonal_code` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pakage_status` int(11) DEFAULT NULL,
+  `pakage_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`, `personal_data_id`, `username`, `role`, `referral_link`) VALUES
-(2, 'admin@gmail.com', '[\"ROLE_ADMIN\"]', '$2y$13$OKjO/hiW3BLHx0Q.YabVz.h2z2tJa2cxrWhSC0axTSHO4YKrSzKPy', 1, 40, NULL, 'ROLE_ADMIN', NULL),
-(3, 'superadmin@gmail.com', '[\"ROLE_SUPER_ADMIN\"]', '$2y$13$LqI6EzlLI5/34oDlfc82wePoTLSZ4jIKDi58MwIojBmfVQgcB7cGm', 1, 35, 'vandelshtam', 'ROLE_ADMIN', NULL);
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`, `personal_data_id`, `username`, `role`, `referral_link`, `pesonal_code`, `pakage_status`, `pakage_id`) VALUES
+(3, '', '0', '0', 0, NULL, 'vandelshtam', 'ROLE_ADMIN', NULL, '2CP1497043565', 1, 0),
+(9, '222@mail.ru', '[\"ROLE_SUPER_ADMIN\"]', '$2y$13$EiQtTwoftE4BtUx/b8AMdO.Z/rUnB58V0hcCiAl8oSFHmvahz039O', 1, 52, NULL, NULL, NULL, '9CP907927910', 1, 313),
+(10, '333@mail.ru', '[\"ROLE_ADMIN\"]', '$2y$13$vMWEDDkM73tw8DQDqTm7q.BBFXq4ApqTIn069o9FYyQ2/ZqYDC/Zi', 1, 53, NULL, NULL, '1-313-313-JAjHikwlH7K0Nl2I19QN', '10CP274970529', 1, 314),
+(11, '444@mail.ru', '[\"ROLE_ADMIN\"]', '$2y$13$0vNFkMy7ilnAjfhvL1rRr.guGA0lHIBPZtd7zENgwLJweDwblM4Qq', 1, 54, NULL, NULL, '1-313-313-JAjHikwlH7K0Nl2I19QN', '11CP1988326848', 1, 315),
+(12, '555@mail.ru', '[]', '$2y$13$K8mQVKG1F590VHRRvuzXUek7el0rfbJFmDfBR1YUFX5eChzb6fwFG', 1, 55, 'Uhfgft', NULL, '1-314-313-JAjHikwlH7K0Nl2I19QN', '12CP1927876844', 1, 316);
 
 -- --------------------------------------------------------
 
@@ -406,7 +487,7 @@ CREATE TABLE `wallet` (
   `etherium` double DEFAULT NULL,
   `bitcoin` double DEFAULT NULL,
   `cometpoin` double DEFAULT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -414,8 +495,10 @@ CREATE TABLE `wallet` (
 --
 
 INSERT INTO `wallet` (`id`, `usdt`, `etherium`, `bitcoin`, `cometpoin`, `user_id`) VALUES
-(1, 1, 1, 1, 1, 2),
-(3, 32, 0, 0, 10000, 3);
+(10, 5000, NULL, NULL, 0, 9),
+(11, 5000, 0, 0, 0, 10),
+(12, 0, 0, 0, 0, 11),
+(13, 10000, 0, 0, 0, 12);
 
 --
 -- Индексы сохранённых таблиц
@@ -426,6 +509,12 @@ INSERT INTO `wallet` (`id`, `usdt`, `etherium`, `bitcoin`, `cometpoin`, `user_id
 --
 ALTER TABLE `doctrine_migration_versions`
   ADD PRIMARY KEY (`version`);
+
+--
+-- Индексы таблицы `fast_consultation`
+--
+ALTER TABLE `fast_consultation`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `list_referral_networks`
@@ -458,12 +547,19 @@ ALTER TABLE `personal_data`
 -- Индексы таблицы `pkege`
 --
 ALTER TABLE `pkege`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8AAF06ACA76ED395` (`user_id`);
 
 --
 -- Индексы таблицы `referral_network`
 --
 ALTER TABLE `referral_network`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `referral_to_email`
+--
+ALTER TABLE `referral_to_email`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -516,10 +612,16 @@ ALTER TABLE `wallet`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `fast_consultation`
+--
+ALTER TABLE `fast_consultation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `list_referral_networks`
 --
 ALTER TABLE `list_referral_networks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT для таблицы `pakages`
@@ -531,13 +633,13 @@ ALTER TABLE `pakages`
 -- AUTO_INCREMENT для таблицы `pakege`
 --
 ALTER TABLE `pakege`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=290;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=317;
 
 --
 -- AUTO_INCREMENT для таблицы `personal_data`
 --
 ALTER TABLE `personal_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT для таблицы `pkege`
@@ -549,6 +651,12 @@ ALTER TABLE `pkege`
 -- AUTO_INCREMENT для таблицы `referral_network`
 --
 ALTER TABLE `referral_network`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT для таблицы `referral_to_email`
+--
+ALTER TABLE `referral_to_email`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -591,7 +699,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT для таблицы `wallet`
 --
 ALTER TABLE `wallet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -614,6 +722,12 @@ ALTER TABLE `pakages`
 --
 ALTER TABLE `pakege`
   ADD CONSTRAINT `FK_ABEDE3E1A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `pkege`
+--
+ALTER TABLE `pkege`
+  ADD CONSTRAINT `FK_8AAF06ACA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `reset_password_request`
